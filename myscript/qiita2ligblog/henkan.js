cfa5c775ede90edb40b509279281974562423018
n0bisuke <rsksound@gmail.com>
2016-01-21 15:18:11 +0900

    md2ligblog

---
+++ myscript/qiita2ligblog/henkan.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let henkan = require('./app');
+
+module.exports = (controller) => {
+  controller.hears(['henkan'],'direct_message,direct_mention,mention', (bot, message) => {
+      bot.api.reactions.add({
+          timestamp: message.ts,
+          channel: message.channel,
+          name: 'robot_face',
+      }, (err, res) => {
+          if (err) bot.botkit.log('Failed to add emoji reaction :(',err);
+      });
+
+      controller.storage.users.get(message.user, (err, user) => {
+          if (user && user.name) {
+              bot.reply(message,'Hello ' + user.name + '!!');
+          } else {
+              //URL抽出
+              let url = message.text.split(' ')[1];
+              //<http://hoge.com>という形式で来るので<>を削除
+              url = url.replace(/[<|>]/gi, '');
+              //ドメイン判定
+              let domain = url.match(/^(.*?:\/\/)(.*?)([a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})[\:[0-9]*]?([\/].*?)?$/i)[3];
+              //qiita以外
+              if(domain !== 'qiita.com'){
+                  bot.reply(message, 'qiitaのみ対応です');
+              }
+              //qiitaなら実行
+              else{
+                bot.reply(message, '変換処理開始\n');
+                henkan(url, (text) => {
+                  bot.reply(message, text);
+                });
+              }
+          }
+      });
+  });
+};
